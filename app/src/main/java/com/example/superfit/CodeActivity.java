26145bc2dbf60e0bff914eaceea00fd188ead8d1
Krasulin <laym245qwe@mail.ru>
2021-04-29 18:19:54 +0300

    Created database that is holding registered users, implemented sign in and sign up mechanics

--- app/src/main/java/com/example/superfit/CodeActivity.java
+++ app/src/main/java/com/example/superfit/CodeActivity.java
@@ -2,14 +2,92 @@ package com.example.superfit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.superfit.DB.SignUpContract;
+import com.example.superfit.DB.SignUpDbHelper;
 
 public class CodeActivity extends AppCompatActivity {
+
+    TextView email;
+    String e_mail, password;
+    SignUpDbHelper dbHelper;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_code);
+
         getSupportActionBar().hide();
+
+        email = findViewById(R.id.emailCodeAc);
+        e_mail = getIntent().getStringExtra("email");
+        dbHelper = new SignUpDbHelper(this);
+
+        email.setText(e_mail);
+        intent = new Intent(this, MainScreen.class);
+    }
+
+    public void backClick(View view){
+        Intent intent = new Intent(this, SignInActivity.class);
+        startActivity(intent);
+    }
+
+    public void onButtonClick(View v){
+        switch (v.getId()){
+            case R.id.b1:
+                password += "1";
+                break;
+            case R.id.b2:
+                password += "2";
+                break;
+            case R.id.b3:
+                password += "3";
+                break;
+            case R.id.b4:
+                password += "4";
+                break;
+            case R.id.b5:
+                password += "5";
+                break;
+            case R.id.b6:
+                password += "6";
+                break;
+            case R.id.b7:
+                password += "7";
+                break;
+            case R.id.b8:
+                password += "8";
+                break;
+            case R.id.b9:
+                password += "9";
+                break;
+        }
+
+        if (password.length() == 4){
+            int digitPassword = Integer.parseInt(password);
+            SQLiteDatabase db = dbHelper.getReadableDatabase();
+            String[] select = {SignUpContract.SignUp.COLUMN_CODE};
+            Cursor cursor = db.query(SignUpContract.SignUp.TABLE_NAME, select, null, null, null, null, null);
+            int codeColumnIndex = cursor.getColumnIndex(SignUpContract.SignUp.COLUMN_CODE);
+            while (cursor.moveToNext()){
+                int currentCode = cursor.getInt(codeColumnIndex);
+                if (digitPassword == currentCode){
+                    startActivity(intent);
+                }
+                else {
+                    Toast.makeText(this, "Invalid password", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            }
+        }
     }
 }
--- app/src/main/java/com/example/superfit/SignInActivity.java
+++ app/src/main/java/com/example/superfit/SignInActivity.java
@@ -3,19 +3,36 @@ package com.example.superfit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 import android.widget.ViewAnimator;
 
+import com.example.superfit.DB.SignUpContract;
+import com.example.superfit.DB.SignUpDbHelper;
+
 public class SignInActivity extends AppCompatActivity {
+
+    EditText userName;
+    SignUpDbHelper dbHelper;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
         getSupportActionBar().hide();
+
+        userName = findViewById(R.id.UN);
+
+        dbHelper = new SignUpDbHelper(this);
+
+        intent = new Intent(this, CodeActivity.class);
     }
 
     public void onSignUpClick(View view){
@@ -24,8 +41,35 @@ public class SignInActivity extends AppCompatActivity {
     }
 
     public void onSignInClick(View view){
-        Intent intent = new Intent(this, MainScreen.class);
-        startActivity(intent);
+        if (validate())
+            checkEmail();
+    }
+
+    public void checkEmail(){
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        String[] select = {SignUpContract.SignUp.COLUMN_EMAIL};
+        Cursor cursor = db.query(SignUpContract.SignUp.TABLE_NAME, select, null, null, null, null, null);
+        int emailColumnIndex = cursor.getColumnIndex(SignUpContract.SignUp.COLUMN_EMAIL);
+        while (cursor.moveToNext()){
+            String currentEmail = cursor.getString(emailColumnIndex);
+            if (userName.getText().toString().equals(currentEmail)){
+                intent.putExtra("email", userName.getText().toString());
+                startActivity(intent);
+                break;
+            }
+            else {
+                Toast.makeText(this, "There is no such Email in our database", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
+
+    public boolean validate (){
+        if (userName.getText().toString().equals("")){
+            Toast.makeText(this, "User Name field is empty", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
     }
 
 }
