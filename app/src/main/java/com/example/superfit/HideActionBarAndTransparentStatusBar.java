2f5c294ceb656bc24d36585536453c423b9a6ade
Vyacheslav <krut.slava@inbox.ru>
2021-04-29 14:08:27 +0300

    Изменил главное окно и добавил класс для скрытия панели навигации и statusBar

---
+++ app/src/main/java/com/example/superfit/HideActionBarAndTransparentStatusBar.java
@@ -0,0 +1,19 @@
+package com.example.superfit;
+
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class HideActionBarAndTransparentStatusBar {
+
+    public static void hide(Window window){
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+        int uiOptions = View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        window.getDecorView().setSystemUiVisibility(uiOptions);
+
+    }
+}
