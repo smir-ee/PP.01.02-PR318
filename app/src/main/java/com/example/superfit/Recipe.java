bf010203d79476a9d45feabd2f550d209c8cee92
Vyacheslav <krut.slava@inbox.ru>
2021-04-29 18:02:02 +0300

    Сделал окно с рецептами, окно одного рецепта и добавил кнопку "Посмотреть все" на главном окне

---
+++ app/src/main/java/com/example/superfit/Recipe.java
@@ -0,0 +1,5 @@
+package com.example.superfit;
+
+public class Recipe {
+    String label, kcal, protein, fat, carbs;
+}
---
+++ app/src/main/java/com/example/superfit/Recipes.java
@@ -0,0 +1,18 @@
+package com.example.superfit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.SearchView;
+import android.widget.ToggleButton;
+
+public class Recipes extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipes);
+
+    }
+}
