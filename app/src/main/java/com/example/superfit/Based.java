80a8a22aa196e422b0c606d262bb63c4036750b4
NikitaDombrovsky <nikita.nikita55@mail.ru>
2021-04-30 13:46:10 +0300

    30.04

--- app/src/main/java/com/example/superfit/Based.java
+++ app/src/main/java/com/example/superfit/Based.java
@@ -6,6 +6,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 
 public class Based extends SQLiteOpenHelper {
+
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "base";
     public static final String TABLE_CONTACTS = "basedb";
